9461dd5f12d226d259fd6786bb3cf1e9ba662504
rrennn08 <66759042+rrennn08@users.noreply.github.com>
2024-02-29 22:31:40 -0800

    OSB-0: Added a few basic chat commands (ping, clear, roll, rpm)

---
+++ oshabott.py
@@ -0,0 +1,25 @@
+import os
+import discord
+import asyncio
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='!', intents=intents)
+
+load_dotenv('token.env')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+@client.event
+async def on_ready():
+    print("Oshabott is now online!")
+
+
+async def main():
+    async with client:
+        await client.load_extension('cogs.chat_commands')
+        await client.start(DISCORD_TOKEN)
+
+asyncio.run(main())
